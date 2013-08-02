cf073543dbc142456d5abac08fcf52aea572a11e
JP <bonnerjp@gmail.com>
2013-08-03 01:07:06 +0800

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odb::Application.config.secret_token = '37d04aabbd4a7aff5c6f29de081796c300ccfdf18d1415e2dfebd6e5ecf39f4e4ab4e5805ed3ffdfa9735cf943cfc2c062cbec5a24ab693227794d0d9c0ff88d'
